86628f6b4ab055ac1e2a0a02f818149bb5cc0bc8
dudihazazi <hazazi.dudi@gmail.com>
2026-01-01 00:04:18 +0900

    feat: zen config

--- hosts/rog-laptop/configuration.nix
+++ hosts/rog-laptop/configuration.nix
@@ -24,6 +24,39 @@
     prime.offload.enableOffloadCmd = true;
   };
 
+  # Zen Browser policies (Firefox-compatible)
+  environment.etc."zen/policies/policies.json".text = builtins.toJSON {
+    policies = {
+      DisableAppUpdate = true;
+      DNSOverHTTPS = {
+        Enabled = true;
+        ProviderURL = "https://dns.quad9.net/dns-query";
+        Locked = true;
+      };
+      ExtensionSettings = {
+        "*" = {
+          installation_mode = "allowed";
+        };
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4629131/ublock_origin-1.68.0.xpi";
+        };
+        "87677a2c52b84ad3a151a4a72f5bd3c4@jetpack" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4585019/grammarly_1-8.934.0.xpi";
+        };
+        "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4455681/traduzir_paginas_web-10.1.1.1.xpi";
+        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4640726/bitwarden_password_manager-2025.12.0.xpi";
+        };
+      };
+    };
+  };
+
   # System version
   system.stateVersion = "25.11";
 }
